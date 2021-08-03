d266b359bbd23c7ee169d1ea8ee8cad6e3ddf722
Psijic <psijic@gmail.com>
2021-08-03 14:02:29 +0300

    up target to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id ("com.android.application")
-    id ("kotlin-android")
+    id("com.android.application")
+    id("kotlin-android")
 //    id ("org.jetbrains.kotlin.plugin.serialization") version kotlin_version
-    id ("com.google.gms.google-services")
-    id ("kotlin-kapt")
+    id("com.google.gms.google-services")
+    id("kotlin-kapt")
 }
 
-apply ("passwords.gradle")
+apply("passwords.gradle")
 
 val googleMapsKey: String by project
 val kotlinVersion: String by project
@@ -22,8 +22,8 @@ android {
         create("release") {
         }
     }
-    compileSdk =30
-    buildToolsVersion ="31.0.0"
+    compileSdk = 31
+    buildToolsVersion = "31.0.0"
 
     buildFeatures {
         viewBinding = true
@@ -31,14 +31,14 @@ android {
     }
 
     defaultConfig {
-        applicationId ="com.psvoid.whappens"
-        minSdk =21
-        targetSdk = 30
-        versionCode =1
-        versionName ="1.0"
+        applicationId = "com.psvoid.whappens"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
         multiDexEnabled = true
 
-        testInstrumentationRunner ="androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders["google_maps_key"] = googleMapsKey
 //        buildConfigField ("String", "EVENTFUL_KEY", "\"$eventful_key\"")
@@ -48,7 +48,7 @@ android {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         getByName("debug") {
 
@@ -68,59 +68,59 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
     // Tests
     testImplementation(kotlin("test"))
-    androidTestImplementation ("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
-    implementation ("com.android.support:multidex:1.0.3")
+    implementation("com.android.support:multidex:1.0.3")
 
     // Data binding
-    kapt ("com.android.databinding:compiler:3.2.0")
+    kapt("com.android.databinding:compiler:3.2.0")
 
     // Material
-    implementation ("com.google.android.material:material:1.4.0")
+    implementation("com.google.android.material:material:1.4.0")
 
-    implementation ("androidx.appcompat:appcompat:1.3.1")
-    implementation ("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.6.0")
 
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
 
-    implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation ("androidx.fragment:fragment-ktx:$fragmentVersion")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.fragment:fragment-ktx:$fragmentVersion")
 
     // Navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:$navigation")
-    implementation ("androidx.navigation:navigation-ui-ktx:$navigation")
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigation")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigation")
 
     // Room
-    implementation ("androidx.room:room-runtime:$roomVersion")
-    implementation ("androidx.room:room-ktx:$roomVersion")
-    kapt ("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
 
     // Maps
-    implementation ("com.google.maps.android:android-maps-utils:2.2.5")
+    implementation("com.google.maps.android:android-maps-utils:2.2.5")
 //    implementation ("com.google.maps.android:android-maps-utils-v3:2.2.5")
-    implementation ("com.google.android.gms:play-services-maps:17.0.1")
+    implementation("com.google.android.gms:play-services-maps:17.0.1")
     //implementation 'com.google.android.gms:play-services-location:17.0.0'
 
     // Firebase
 //    implementation 'com.google.firebase:firebase-analytics:17.4.0'
 //    implementation 'com.google.firebase:firebase-storage:19.1.1'
 //    implementation 'com.google.firebase:firebase-messaging:20.1.6'
-    implementation ("com.google.firebase:firebase-database-ktx:20.0.1")
+    implementation("com.google.firebase:firebase-database-ktx:20.0.1")
 //    implementation 'com.google.firebase:firebase-auth:19.3.1'
 //    implementation 'com.google.firebase:firebase-config:19.1.4'
 //    implementation 'com.firebaseui:firebase-ui-auth:6.2.1'
 
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
 
-    implementation ("com.jakewharton.timber:timber:$timberVersion")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.jakewharton.timber:timber:$timberVersion")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 
 }
